fbc8e5d98ecb90739eaa44a86e1e850597cbe970
0yunse0 <yunny9624@naver.com>
2024-11-21 23:59:54 +0900

    sign up with DB

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const config = require("./config");
+const userRoutes = require("./model/user-routes");
+
+
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+// 정적 파일 제공 (public 디렉토리)
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// 기본 경로로 요청 시 login.html 제공
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+// API 라우트
+app.use("/api", userRoutes.routes);
+
+// 서버 시작
+app.listen(config.port, () =>
+  console.log("App is Listening on url http://localhost:" + config.port)
+);
---
+++ model/user.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(ID, PW, email, name) {
+        this.ID = ID;
+        this.PW = PW;
+        this.email = email;
+        this.name = name;
+
+    }
+}
