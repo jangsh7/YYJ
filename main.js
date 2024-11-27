a71e98b22d9cf2a996e56d70f10a592665b778c3
choehyeonjin <guswls2616@naver.com>
2024-11-27 17:08:53 +0900

    Feat: find id

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const config = require("./config");
 const userRoutes = require("./model/user-routes");
+const nodemailer = require("nodemailer");
 
 app.use(express.json());
 app.use(cors());
@@ -17,7 +18,6 @@ app.use(bodyParser.json());
 // 정적 파일 제공 설정
 app.use(express.static('public'));
 
-
 // 기본 경로로 요청 시 login.html 제공
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "login.html"));
@@ -25,6 +25,37 @@ app.get("/", (req, res) => {
 
 // API 라우트
 app.use("/api", userRoutes.routes);
+
+// Nodemailer 설정
+const transporter = nodemailer.createTransport({
+    service: "gmail", // Gmail 사용
+    auth: {
+        user: "ossyyj@gmail.com", // 발신 이메일 주소
+        pass: "yxwv vupw rzct drqz", // 앱 비밀번호
+    },
+});
+
+// 아이디 전송 엔드포인트
+app.post("/send-id", (req, res) => {
+    const { email, userId } = req.body;
+
+    const mailOptions = {
+        from: "ossyyj@gmail.com",
+        to: email,
+        subject: "요청하신 아이디입니다",
+        text: `안녕하세요. 요청하신 아이디는 "${userId}"입니다.`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error("Error sending email:", error);
+            res.status(500).send("Email not sent");
+        } else {
+            console.log("Email sent:", info.response);
+            res.status(200).send("Email sent");
+        }
+    });
+});
 
 // 서버 시작
 app.listen(config.port, () =>
