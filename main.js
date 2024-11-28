ac2cff1edd160c0c2f253bf49f4f5cb02bb09b7f
choehyeonjin <guswls2616@naver.com>
2024-11-28 13:54:41 +0900

    Feat: find password

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ app.use("/api", userRoutes.routes);
 
 // Nodemailer 설정
 const transporter = nodemailer.createTransport({
-    service: "gmail", // Gmail 사용
+    service: "gmail", // gmail 사용
     auth: {
         user: "ossyyj@gmail.com", // 발신 이메일 주소
         pass: "yxwv vupw rzct drqz", // 앱 비밀번호
@@ -42,8 +42,8 @@ app.post("/send-id", (req, res) => {
     const mailOptions = {
         from: "ossyyj@gmail.com",
         to: email,
-        subject: "요청하신 아이디입니다",
-        text: `안녕하세요. 요청하신 아이디는 "${userId}"입니다.`,
+        subject: "[요야정] 요청하신 아이디입니다.",
+        text: `안녕하세요! 요청하신 아이디는 다음과 같습니다:\n\n아이디: ${userId}`,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
@@ -53,6 +53,28 @@ app.post("/send-id", (req, res) => {
         } else {
             console.log("Email sent:", info.response);
             res.status(200).send("Email sent");
+        }
+    });
+});
+
+// 비밀번호 전송 엔드포인트
+app.post("/send-password", (req, res) => {
+    const { email, password } = req.body;
+
+    const mailOptions = {
+        from: "your-email@gmail.com",
+        to: email,
+        subject: "[요야정] 요청하신 비밀번호입니다.",
+        text: `안녕하세요! 요청하신 비밀번호는 다음과 같습니다:\n\n비밀번호: ${password}\n\n비밀번호는 외부에 유출되지 않도록 주의해주세요.`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error("Error sending email:", error);
+            res.status(500).send("Failed to send email.");
+        } else {
+            console.log("Email sent successfully:", info.response);
+            res.status(200).send("Email sent successfully.");
         }
     });
 });
